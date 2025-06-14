39cf32f53c3d20cfc3684b83dbacb11d6780df76
toothles-ai <tharushanamadara0000@gmail.com>
2025-06-14 20:11:20 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? '𝐒𝐔𝐋𝐀-𝐌𝐃=hv9yHSCI#AOETI75KtMxdzeENn00irKm_Qcfi7JRAEHufeaQQTRE' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝐒𝐔𝐋𝐀-𝐌𝐃=JiszUKzL#ROPsltj4NNQVu1NV3f92COXv3wWgwCvhXnlWT01QGKo' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS === undefined ?"true" : process.env.AUTO_READ_STATUS,
 MODE: process.env.MODE === undefined ?"privet" : process.env.MODE,
